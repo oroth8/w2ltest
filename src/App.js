f8122b28e06361708bf1ac92c1fa5f5be9e66cdf
oroth8 <oroth8@comcast.net>
2021-02-19 15:48:12 -0600

    added usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [formData, setFormData] = useState("");
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
   return (
     <div className="App">
       <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="first_name" />
+        <input type="hidden" name="form-name" />
+        <input
+          type="text"
+          name="first_name"
+          value={formData}
+          onChange={(e) => setFormData(e.target.value)}
+        />
         <button type="submit">Send</button>
       </form>
     </div>
