8adbe65c249ea75afdde349de5bf0fa3cfe3a3aa
oroth8 <oroth8@comcast.net>
2021-02-21 10:56:23 -0600

    error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ContactForm from "./Form";
 
 function App() {
-  const [formData, setFormData] = useState("");
-
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
   return (
     <div className="App">
       <ContactForm />
