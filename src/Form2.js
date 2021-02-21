0033e87756f3bac6502af40cab60a352ca999224
Owen Roth <rothowen27@gmail.com>
2021-02-21 13:22:17 -0600

    spread

--- src/Form2.js
+++ src/Form2.js
@@ -21,9 +21,10 @@ const Form2 = () => {
       fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", formData }),
+        body: encode({ "form-name": "contact", ...formData }),
       });
       console.log("success");
+      console.log(formData);
     } catch (error) {
       console.log(error);
     }
