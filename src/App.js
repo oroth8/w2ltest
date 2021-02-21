c4056ea3871355d2368f2cd4ee6b91e108343e9f
Owen Roth <rothowen27@gmail.com>
2021-02-21 16:01:09 -0600

    render 3

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Form2 from "./Form2";
 import ContactForm from "./Form";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<Form2 />, document.getElementById("root"));
-
 function App() {
+  ReactDOM.render(<Form2 />, document.getElementById("root"));
   return (
     <div className="App">
       <Form2 />
